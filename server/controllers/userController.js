5cf7f623009ff50c88cc5b58763524399ebe9f34
WolfMAN <aria.azadi.pour@protonmail.com>
2020-06-27 01:10:49 +0430

    save user avatar image

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,3 +1,5 @@
+const sharp = require('sharp');
+
 const factory = require('./handlerFactory');
 const User = require('../models/User');
 const catchRequest = require('../utils/catchRequest');
@@ -16,5 +18,21 @@ exports.setUserType = catchRequest(
     (req, res, next) => {
         req.body.rote = undefined;
         next();
+    }
+);
+
+exports.saveAvatar = catchRequest(
+    async (req, res, next) => {
+        if (!req.file) {
+            return next();
+        }
+        const ext = req.file.mimetype.split('/')[1];
+        req.file.filename = `user-avatar-${req.user.id}-${Date.now()}.${ext}`;
+        req.body.avatarImage = req.file.filename;
+        await sharp(req.file.buffer)
+            .resize(500, 500)
+            .toFormat('jpeg')
+            .jpeg({quality: 90})
+            .toFile(`uploads/useravatar/${req.files.avatarImage[0].filename}`);
     }
 );
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
+const uploadImage = require('../utils/uploadImage');
 
 const router = express.Router();
 
@@ -11,11 +12,11 @@ router.route('/auth/signin').post(authController.signIn);
 
 router.route('/')
     .get(userController.getUsers)
-    .post(authController.protect, userController.setUserType, userController.createUser);
+    .post(authController.protect, userController.setUserType, uploadImage.single('avatar'), userController.saveAvatar, userController.createUser);
 
 router.route('/:id')
     .get(userController.getUser)
-    .patch(authController.protect, authController.restrictTo('admin', 'selfUser', 'selfManager'), userController.setUserType, userController.updateUser)
+    .patch(authController.protect, authController.restrictTo('admin', 'selfUser', 'selfManager'), userController.setUserType, uploadImage.single('avatar'), userController.saveAvatar, userController.updateUser)
     .delete(authController.protect, authController.restrictTo('admin', 'selfUser', 'selfManager'),  userController.deleteUser);
 
 module.exports = router;
