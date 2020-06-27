1cb55cc1a3089f2b1e24409774fce87b0f450448
WolfMAN <aria.azadi.pour@protonmail.com>
2020-06-27 21:41:18 +0430

    save images in product controller

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,5 +1,8 @@
+const sharp = require('sharp');
+
 const factory = require('./handlerFactory');
 const Product = require('../models/Product');
+const catchRequest = require('../utils/catchRequest');
 
 exports.getProducts = factory.getAll(Product);
 
@@ -10,3 +13,20 @@ exports.getProduct = factory.getOne(Product);
 exports.updateProduct = factory.updateOne(Product);
 
 exports.deleteProduct = factory.deleteOne(Product);
+
+exports.saveImages = catchRequest(
+    async (req, res, next) => {
+        if (req.files) {
+            for (let i = 0; i < req.files.length; i++) {
+                const ext = req.files[i].mimetype.split('/')[1];
+                req.files[i].filename = `product-image-${req.user.id}-${Date.now()}.${ext}`;
+                req.body.images[i] = req.files[i].filename;
+                await sharp(req.files[i].buffer)
+                    .resize(500, 500)
+                    .toFormat('jpeg')
+                    .jpeg({quality: 90})
+                    .toFile(`uploads/useravatar/${req.files.avatarImage[0].filename}`);
+            }
+        }
+    }
+);
